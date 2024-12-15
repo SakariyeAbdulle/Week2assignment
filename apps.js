7e4fb8737cf4e4f2373deb5cbca276def3ea0f50
SAKARIYEABDULLE <ZAKARIYAABDULLE@GMAIL.COM>
2024-12-15 19:49:58 +0000

    finished doing the thumbnail container

---
+++ apps.js
@@ -0,0 +1,30 @@
+
+ console.log("test");
+
+  const images = [
+    { src: "./tiger.png", alt: "a rare image of a white tiger" },
+    { src: "./wild.png", alt: "a image of a donkey known as the somali wild ass" },
+    { src: "./bear.png", alt: "a image of a bear in the wild" },
+    { src: "./fox.png", alt: "a image of a fox in its natural habitat" },
+    {src:"./dg.png", alt: "a image of ,a dalmation dog in spring" },
+  ];
+  const thumbnail= document.getElementById ("thumbnail-container");
+
+
+function  CreateThumbnail(imagesArray){  for (imagesArray=0; imagesArray<images.length;imagesArray++){
+  console.log(images[imagesArray]);
+  const newImage = document.createElement("img");
+  newImage.src = images[imagesArray].src;
+    newImage.alt = images[imagesArray].alt;
+  newImage.className = "ImageStyle";
+  thumbnail.appendChild(newImage);
+  // add event here 
+
+};}
+ 
+CreateThumbnail(images);
+function createLargeImagesHandler(largeImage){
+  largeImageContainer.inner.HTML = null
+
+}
+  
